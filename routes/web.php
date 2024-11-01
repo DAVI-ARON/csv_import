a5c61b21583ad2899ee54b67ea49c3cb1a6127d1
Davi_Aron <desenvolvedor01@zamed.com.br>
2024-11-01 16:27:39 -0300

    criando rota para importacao de arquivos

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\UserController;
+use App\Http\Controllers\UserController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Route::get('/', [UserController::Class, 'index'])->name('user.index');
+Route::post('/users', [UserController::Class, 'import'])->name('user.import');
