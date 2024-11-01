2e312250acd44ff1a2a9f85e62df463eab905bf8
Davi_Aron <desenvolvedor01@zamed.com.br>
2024-11-01 16:26:50 -0300

    adicionando funcao import para captar o valor dos arquivos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    // Litsar Usuários
+    public function index() {
+
+        // recuperar registros do banco de dados
+        $users = User::get();
+
+        return view('users.index', ['users' => $users]);
+
+    }
+
+    // Importando arquivos
+    public function import(Request $request) {
+        dd($request);
+    }
 }
