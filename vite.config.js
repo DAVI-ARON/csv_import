53cc7b37d2963e32a0b152a7613a55d3324ce42f
Davi_Aron <desenvolvedor01@zamed.com.br>
2024-11-01 13:49:32 -0300

    configurando vite para atualizacoes automaticas

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  server: {
+    hmr: {
+      host: '192.168.1.145',
+    },
+  },
+  plugins: [
+      laravel({
+          input: [
+              'resources/css/app.css',
+              'resources/js/app.js',
+          ],
+          refresh: true,
+      }),
+  ],
 });
